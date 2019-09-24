86fc513fb92e5ab10f9a278e30881124620a9de8
refka-farid <refka.farid@gmail.com>
2019-09-24 13:07:49 +0200

    [learning kotlin]  chapter 7 noteKeeper app done

--- Google Associate Android Developer (AAD)/1-Beginner/1-Android Apps with Kotlin Build Your First App/note-keeper-kotlin/app/src/main/java/com/bravedroid/notekeeper/MainActivity.kt
+++ Google Associate Android Developer (AAD)/1-Beginner/1-Android Apps with Kotlin Build Your First App/note-keeper-kotlin/app/src/main/java/com/bravedroid/notekeeper/MainActivity.kt
@@ -25,12 +25,17 @@ class MainActivity : AppCompatActivity() {
 
         spinnerCourses.adapter = adapterCourses
 
-        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        notePosition =
+            savedInstanceState?.getInt(NOTE_POSITION, POSITION_NOT_SET) ?: intent.getIntExtra(
+                NOTE_POSITION,
+                POSITION_NOT_SET
+            )
+
         if (notePosition != POSITION_NOT_SET)
             displayNote()
-        else{
+        else {
             DataManager.notes.add(NoteInfo())
-            notePosition=DataManager.notes.lastIndex
+            notePosition = DataManager.notes.lastIndex
         }
     }
 
@@ -87,8 +92,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveNote() {
         val note = DataManager.notes[notePosition]
-        note.title=textNoteTitle.text.toString()
-        note.text=textNoteText.text.toString()
-        note.course=spinnerCourses.selectedItem as CourseInfo
+        note.title = textNoteTitle.text.toString()
+        note.text = textNoteText.text.toString()
+        note.course = spinnerCourses.selectedItem as CourseInfo
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(NOTE_POSITION, notePosition)
     }
 }
