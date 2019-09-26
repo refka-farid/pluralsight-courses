eeba1c1e370d5af1ac7d69ed549dc8994a12df95
refka-farid <refka.farid@gmail.com>
2019-09-26 12:58:54 +0200

    [beginner-2-java-test]  android tools and testing java project

--- Google Associate Android Developer (AAD)/1-Beginner/2-Working with Android Tools and Testing/note-keeper-java/app/src/androidTest/java/com/bravedroid/notekeeper/NoteCreationTest.java
+++ Google Associate Android Developer (AAD)/1-Beginner/2-Working with Android Tools and Testing/note-keeper-java/app/src/androidTest/java/com/bravedroid/notekeeper/NoteCreationTest.java
@@ -14,7 +14,12 @@ import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.core.AllOf.allOf;
@@ -36,7 +41,6 @@ public class NoteCreationTest {
 
     @Test
     public void createNewNote() throws InterruptedException {
-        //GIVEN
         final CourseInfo course = sDataManager.getCourse("java_lang");
         final String noteTitle = "Test note title";
         final String noteText = "this is the body of our test note";
@@ -44,16 +48,25 @@ public class NoteCreationTest {
         //fabNewNote.perform((click()));
         onView(withId(R.id.fab)).perform(click());
         onView(withId(R.id.spinner_courses)).perform(click());
-        //WHEN
         onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
-        // Thread.sleep(33000);
-        onView(withId(R.id.text_note_title))
-                .perform(typeText(noteTitle));
-        onView(withId(R.id.text_note_text))
-                .perform(typeText(noteText),
-                        closeSoftKeyboard());
+
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle()))));
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+                .check(matches(withText(containsString(noteTitle))));
+
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
+                closeSoftKeyboard());
+
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
         pressBack();
-        //THEN
-        // TODO: 25/09/2019  
+
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+        
     }
 }
